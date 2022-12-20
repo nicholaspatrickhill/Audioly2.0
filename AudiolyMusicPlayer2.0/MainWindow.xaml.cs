07bb9ee7e7da2b70f23ef5fcad83b416b6eb99a0
Nick Hill <nicholaspatrickhill@gmail.com>
2022-12-19 23:25:33 -0500

    added several UI elements & event handling for open file

--- AudiolyMusicPlayer2.0/MainWindow.xaml.cs
+++ AudiolyMusicPlayer2.0/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,32 @@ namespace AudiolyMusicPlayer2._0
         public MainWindow()
         {
             InitializeComponent();
+            string ImgPath = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\MusicIcon.png";
+            Musicimg.Source = new BitmapImage(new Uri(ImgPath));
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog= new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                //lblSongname.Text = openFileDialog.FileName;
+                lblSongname.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void Card_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
